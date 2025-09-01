a892bd489dfd9f76c76d623ee70c58c28b6b2ce1
ristoph <christowrites@gmail.com>
2025-09-01 19:07:29 +0530

    backend: create add new todo route

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,36 @@
 const express = require("express")
 const jwt  = require("jwt")
-const mng = require("mongoose")
+const {todo} = require("./db")
+const cors = require("cors")
+const {createTodo, updateTodo} = require("./types")
 const app = express()
 
 app.use(express.json())
 
+app.post( "/todo", async function(req,res){
+    const createPayload = req.body
+    const paresedPayload = createTodo.safeparse(createPayload)
+
+    if(!paresedPayload.success){
+        res.status(411).json({
+            msg: "Inputs invalid",
+        })
+        return;
+    }
+
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+
+    res.json({
+        msg: "Todo created"
+    })
+})
+
+app.get("/todos",
+)
+
+app.post("/completed",
+)
